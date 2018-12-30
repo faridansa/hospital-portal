c0d719fe094fc215cbadf46faa11307e792ce2e4
faridansa <amirahmandani@gmail.com>
2018-12-31 00:11:21 +0700

    get post request from selected province

--- app.py
+++ app.py
@@ -5,27 +5,32 @@ app = Flask(__name__)
 app._static_folder = 'static/'
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
     data_provinsi = get_seluruh_provinsi()
-    return render_template('homepage.html', provinsi=data_provinsi)
+    if request.method == 'GET':
+        print('here')
+        return render_template('homepage.html', provinsi=data_provinsi)
+    else:
+        selected_provinsi = request.form.get('dropdown')
+        return data_rs_provinsi_pengelola(selected_provinsi)
 
-
-@app.route("/data-rs/<provinsi>/statistik-tenaga-medis")
-def data_rs_provinsi_statisik_tenaga_medis(provinsi):
-    tabel = get_tabel_statistik_tenaga_medis(provinsi)
-    return render_template('data-rs-provinsi.html', provinsi=provinsi, tabel_stm=tabel)
-
-
-@app.route("/data-rs/<provinsi>/tipe-perawatan")
-def data_rs_provinsi_tipe_perawatan(provinsi):
-    tabel = get_tipe_perawatan(provinsi)
-    return render_template('data-rs-provinsi.html', provinsi=provinsi, tabel_tipe_perawatan=tabel)
+#
+# @app.route("/data-rs/<provinsi>/statistik-tenaga-medis")
+# def data_rs_provinsi_statisik_tenaga_medis(provinsi):
+#     tabel = get_tabel_statistik_tenaga_medis(provinsi)
+#     return render_template('data-rs-provinsi.html', provinsi=provinsi, tabel_stm=tabel)
+#
+#
+# @app.route("/data-rs/<provinsi>/tipe-perawatan")
+# def data_rs_provinsi_tipe_perawatan(provinsi):
+#     tabel = get_tipe_perawatan(provinsi)
+#     return render_template('data-rs-provinsi.html', provinsi=provinsi, tabel_tipe_perawatan=tabel)
 
 
 @app.route("/data-rs/<provinsi>/pengelola")
 def data_rs_provinsi_pengelola(provinsi):
-    tabel = get_pengelolan(provinsi)
+    tabel = get_tabel_pengelola(provinsi)
     return render_template('data-rs-provinsi.html', provinsi=provinsi, tabel_pengelola=tabel)
 
 
@@ -58,17 +63,17 @@ def kelas_rs(provinsi, kelas_rs):
     tabel = get_tabel_kelas_rs(kelas_rs, provinsi)
     return render_template('kelas-rs.html', provinsi=provinsi, kelas_rs=kelas_rs, tabel_kelas_rs=tabel)
 
-
-@app.route("/data-rs/<wilayah>/jumlah-rs")
-def jumlah_kamar_rs(wilayah):
-    jumlah_kamar, tabel = get_tabel_rs_jumlah_kamar(wilayah)
-    return render_template('jumlah-kamar.html', wilayah=wilayah, jumlah_kamar=jumlah_kamar, tabel_rs=tabel)
-
-
-@app.route("/data-puskesmas/detail/<puskesmas>")
-def detail_puskesmas(puskesmas):
-    data = get_detail_puskesmas(puskesmas)
-    return render_template('detail-puskesmas.html', puskesmas=puskesmas, detail=data)
+#
+# @app.route("/data-rs/<wilayah>/jumlah-rs")
+# def jumlah_kamar_rs(wilayah):
+#     jumlah_kamar, tabel = get_tabel_rs_jumlah_kamar(wilayah)
+#     return render_template('jumlah-kamar.html', wilayah=wilayah, jumlah_kamar=jumlah_kamar, tabel_rs=tabel)
+#
+#
+# @app.route("/data-puskesmas/detail/<puskesmas>")
+# def detail_puskesmas(puskesmas):
+#     data = get_detail_puskesmas(puskesmas)
+#     return render_template('detail-puskesmas.html', puskesmas=puskesmas, detail=data)
 
 
 if __name__ == "__main__":
